3b36c5fc140b53349748cc8504d3ab524af3d399
Joanne Longeville <joanne.longeville@gmail.com>
2024-06-09 17:08:33 +0200

    tidy database

--- app/src/connectors/database.py
+++ app/src/connectors/database.py
@@ -1,31 +1,37 @@
 import psycopg2
-# def main():
 
-# def create_table():
-# def read_table():
-# def update_table():
-# def delete_table():
+# Database connection
+def db_connection(): 
+    conn = psycopg2.connect(user="postgres",
+                            password="admin",
+                            host="localhost",
+                            port="5432",
+                            database="sportfirst")
+    return conn
 
 def create_table():
     try:
-        # Connexion à la base de données
-        conn = psycopg2.connect(user="postgres",
-                                password="admin",
-                                host="localhost",
-                                port="5432",
-                                database="sportfirst")
-        
-        # Créer un curseur pour exécuter des commandes SQL
+        # Database connection
+        conn = db_connection()
+
+        # Create a cursor to execute SQL commands
         cur = conn.cursor()
 
         create_users_table = ''' CREATE TABLE IF NOT EXISTS Users (
-            user_Id INT PRIMARY KEY NOT NULL,
-            name TEXT NOT NULL,
-            last_name TEXT NOT NULL,
+            user_Id SERIAL PRIMARY KEY,
+            firstname TEXT NOT NULL,
+            lastname TEXT NOT NULL,
             email TEXT NOT NULL,
             password TEXT NOT NULL,
-            phone_number TEXT NOT NULL,
-            role TEXT NOT NULL
+            phone TEXT NOT NULL,
+            role TEXT
+            ) '''
+        
+        create_roles_table = ''' CREATE TABLE IF NOT EXISTS Roles (
+            role_id SERIAL PRIMARY KEY,
+            role TEXT NOT NULL,
+            user_Id INT NOT NULL,
+            FOREIGN KEY (user_id) REFERENCES Users(user_id)
             ) '''
         
         create_sessions_table = ''' CREATE TABLE IF NOT EXISTS Sessions (
@@ -44,21 +50,17 @@ def create_table():
             FOREIGN KEY (user_id) REFERENCES Users(user_id)
             ) '''
 
-        # Exécuter la commande SQL
+        # Execute SQL command
         cur.execute(create_users_table)
-        print("Table 'Users' created successfully")
-
+        cur.execute(create_roles_table)
         cur.execute(create_sessions_table)
-        print("Table 'Sessions' created successfully")
-
         cur.execute(create_availabilities_table)
-        print("Table 'Availabilities' created successfully")
 
-        # Valider les modifications
+        # Validate changes
         conn.commit()
-        print("Tables created successfully")
+        print("Tables 'Users', 'Roles' 'Sessions' and 'Availabilities' created successfully")
 
-        # Fermer la connexion a la base de données
+        # Close database connection
         cur.close()
         conn.close()
         print("PostgreSQL connection is closed")
