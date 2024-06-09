e0be66f2c830beceec0440b7918e3e3b17abd0e7
Joanne Longeville <joanne.longeville@gmail.com>
2024-06-09 17:09:55 +0200

    add flask to post datas

--- app/src/main.py
+++ app/src/main.py
@@ -1,6 +1,22 @@
+
 from connectors import database
+from connectors.authentication import handle_register_post
+from http.server import HTTPServer, BaseHTTPRequestHandler
+from middleware import handle_cors_headers
+from flask import Flask, request
+from flask_restful import Resource, Api
 
-def main():
+app = Flask(__name__)
+api = Api(app)
+
+class AccountResource(Resource):
+    def post(self):
+        data = request.get_json()
+        response_data, response_code = handle_register_post(data)
+        return response_data, response_code
+
+api.add_resource(AccountResource, '/accounts')
+
+if __name__ == "__main__":
     database.create_table()
-
-main()
+    app.run(debug=True)
