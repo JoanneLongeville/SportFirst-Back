436b72f95ac9c399c98330eef8236f4e80db4c27
Joanne Longeville <joanne.longeville@gmail.com>
2024-06-13 12:12:47 +0200

    replace middleware.py by flask

--- app/src/middleware.py
+++
@@ -1,4 +0,0 @@
-def handle_cors_headers(handler):
-    handler.send_header('Access-Control-Allow-Origin', 'http://localhost:8080')
-    handler.send_header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
-    handler.send_header('Access-Control-Allow-Headers', 'Content-Type, Content-Length')
